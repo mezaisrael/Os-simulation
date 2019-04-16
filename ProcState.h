8ba320d4e6e0737cbf2f6ff54be0d51aa04e368e
mezaisrael <meza.israel.91@gmail.com>
2019-04-16 16:18:19 -0400

    rename pState to ProcSate

---
+++ ProcState.h
@@ -0,0 +1,10 @@
+//
+// Created by israel on 4/16/19.
+//
+
+#ifndef UNTITLED_PROCSTATE_H
+#define UNTITLED_PROCSTATE_H
+
+enum ProcState{ready, waiting, running, terminated};
+
+#endif //UNTITLED_PROCSTATE_H
